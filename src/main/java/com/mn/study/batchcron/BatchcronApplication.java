dd74dd6d6e8bd530eab34c51817532d00871476e
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2020-08-20 10:52:48 +0200

    init project

---
+++ src/main/java/com/mn/study/batchcron/BatchcronApplication.java
@@ -0,0 +1,13 @@
+package com.mn.study.batchcron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchcronApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatchcronApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mn/study/batchcron/BatchcronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mn.study.batchcron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchcronApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
