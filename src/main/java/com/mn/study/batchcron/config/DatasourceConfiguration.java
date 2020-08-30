5926893012c0cffaba3d1ee1cf3ed5a15c968b08
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2020-08-30 10:22:19 +0200

    add dataconfig

--- src/main/java/com/mn/study/batchcron/config/DatasourceConfiguration.java
+++ src/main/java/com/mn/study/batchcron/config/DatasourceConfiguration.java
@@ -12,9 +12,11 @@ public class DatasourceConfiguration {
 
   private static final String FORUM_DATABASE = "FORUM_DATABASE";
 
+  private static final String VIDEO_DATABASE = "VIDEO_DATABASE";
+
   @Bean(name = FORUM_DATABASE)
   @Primary
-  public DataSource dataSource() {
+  public DataSource forumDataSource() {
     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
     dataSourceBuilder.url("jdbc:postgresql://localhost:5432/forumapi");
     dataSourceBuilder.username("postgres");
@@ -22,4 +24,12 @@ public class DatasourceConfiguration {
     return dataSourceBuilder.build();
   }
 
+  @Bean(name = VIDEO_DATABASE)
+  public DataSource videoDataSource() {
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.url("jdbc:postgresql://localhost:5432/videorental");
+    dataSourceBuilder.username("postgres");
+    dataSourceBuilder.password("1109");
+    return dataSourceBuilder.build();
+  }
 }
